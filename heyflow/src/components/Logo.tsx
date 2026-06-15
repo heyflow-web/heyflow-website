8c2c5265ee72332be11029cf40b2f8af11245187
myeon <myeon@myeonui-iMac.local>
2026-06-15 20:00:32 +0900

    feat: replace svg logo with original brand image file

--- heyflow/src/components/Logo.tsx
+++ heyflow/src/components/Logo.tsx
@@ -2,22 +2,9 @@ import React from 'react';
 
 export default function Logo({ className = "", style = {} }: { className?: string; style?: React.CSSProperties }) {
   return (
-    <div className={`logo-container ${className}`} style={{ display: 'inline-flex', alignItems: 'center', gap: '8px', ...style }}>
-      <svg width="34" height="20" viewBox="0 0 40 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style={{ flexShrink: 0 }}>
-        {/* Square */}
-        <rect x="0" y="0" width="24" height="24" />
-        {/* Half Circle (curved on left, flat on right) */}
-        <path d="M 40 0 A 12 12 0 0 0 40 24 Z" />
-      </svg>
-      <span style={{ 
-        fontFamily: 'serif', 
-        fontSize: '1.5rem', 
-        lineHeight: 1,
-        letterSpacing: '0.02em',
-        fontWeight: 500
-      }}>
-        Hey<span style={{ fontStyle: 'italic', fontWeight: 500 }}>Flow</span>
-      </span>
+    <div className={`logo-container ${className}`} style={{ display: 'inline-flex', alignItems: 'center', ...style }}>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="/logo.png" alt="HeyFlow Logo" style={{ height: '24px', width: 'auto', flexShrink: 0 }} />
     </div>
   );
 }
